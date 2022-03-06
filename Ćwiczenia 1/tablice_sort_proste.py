af7ae245609d4b18357ce9cfe2ad17e32505d103
Nepommuck <100998310+Nepommuck@users.noreply.github.com>
2022-03-06 20:39:41 +0100

    Create tablice_sort_proste.py

---
+++ Ćwiczenia 1/tablice_sort_proste.py
@@ -0,0 +1,29 @@
+def bubble(tab, l):
+    for i in range(l-1, -1, -1):
+        for k in range(i):
+            if tab[k] > tab[k+1]:
+                tab[k], tab[k+1] = tab[k+1], tab[k]
+                print(tab)
+            else:
+                print("pass")
+
+
+def insersion(tab, l):
+    for i in range(1, l):
+        buf = tab[i]
+        k = i-1
+        while tab[k] > buf and k >= 0:
+            tab[k+1] = tab[k]
+            k -= 1
+        tab[k+1] = buf
+
+
+def selection(tab, l):
+    for i in range(l-1):
+        ind = i+1
+        min = tab[ind]
+        for k in range(i+2, l):
+            if tab[k] < min:
+                ind = k
+                min = tab[k]
+        tab[ind], tab[i] = tab[i], tab[ind]
