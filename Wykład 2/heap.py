f06a830a44debc5bf883535dd90ff15d65636390
Nepommuck <100998310+Nepommuck@users.noreply.github.com>
2022-03-09 19:24:36 +0100

    Create heap.py

---
+++ Wykład 2/heap.py
@@ -0,0 +1,38 @@
+def swap(tab, i, j):
+    tab[i], tab[j] = tab[j], tab[i]
+
+def left(i):
+    return 2*i + 1
+
+def right(i):
+    return 2*i + 2
+
+def parent(i):
+    return (i-1) // 2
+
+
+def heapify(T, n, i):
+    mx = i
+    l = left(i)
+    r = right(i)
+    if l < n and T[l] > T[mx]:
+        mx = l
+    if r < n and T[r] > T[mx]:
+        mx = r
+    if mx > i:
+        swap(T, i, mx)
+        heapify(T, n, mx)
+
+
+def build_heap(T):
+    n = len(T)
+    for i in range(parent(n-1), -1, -1):
+        heapify(T, n, i)
+
+
+def heapsort(T):
+    n = len(T)
+    build_heap(T)
+    for i in range(n-1, 0, -1):
+        swap(T, 0, i)
+        heapify(T, i, 0)
